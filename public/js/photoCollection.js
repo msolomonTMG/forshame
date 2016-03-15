fb043b499f5f2811c0bdadf3e53fb99ab888c27f
Mike Solomon <msolomon@thrillist.com>
2016-03-15 13:33:29 -0400

    add skae image

--- public/js/photoCollection.js
+++ public/js/photoCollection.js
@@ -4,6 +4,11 @@ var photoCollection = {
       "https://media.giphy.com/media/41zG0LWQlPTAQ/giphy.gif"
     ]
   },
+  "cskae": {
+    "photos": [
+      "/img/cskae/img_1.png"
+    ]
+  },
   "mhaarhaus": {
     "photos": [
       "https://media.giphy.com/media/O60o6B2N2ZMxG/giphy.gif"
